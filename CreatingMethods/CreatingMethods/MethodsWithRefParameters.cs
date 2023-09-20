1d574a274bc959af12958a5cf1d97de70dd623b4
xusanchik <samadovxusan9013@gmail.com>
2023-09-20 22:04:01 +0500

    create methods

--- CreatingMethods/CreatingMethods/MethodsWithRefParameters.cs
+++ CreatingMethods/CreatingMethods/MethodsWithRefParameters.cs
@@ -1,20 +1,58 @@
-﻿namespace CreatingMethods
+﻿using System.IO;
+
+namespace CreatingMethods
 {
     public static class MethodsWithRefParameters
     {
-        //public static bool ReturnParameterValueAndSetParameterToDefaultValue(ref bool boolValue)
-        //{
-        //    bool value = boolValue;
-        //    boolValue = default(bool);
-        //    return value;
-        //}
+        public static bool ReturnParameterValueAndSetParameterToDefaultValue(ref bool boolValue)
+        {
+            bool value = boolValue;
+            boolValue = default(bool);
+            return value;
+        }
 
-        // TODO #17. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue" that gets "charValue" parameter ("char" type) with ref keyword. The method should return value of "charValue" parameter and set the "charValue" parameter to default value (use "default" operator).
+        // TODO #17. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue"
+        // that gets "charValue" parameter ("char" type) with ref keyword. The method should return value of "charValue"
+        // parameter and set the "charValue" parameter to default value (use "default" operator).
+        public static char ReturnParameterValueAndSetParameterToDefaultValue(ref char charValue)
+        {
+            char defaultValue = default;
+            char returnValue = charValue;
+            charValue = defaultValue;
+            return returnValue;
+        }
 
-        // TODO #18. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue" that gets "floatValue" parameter ("float" type) with ref keyword. The method should return value of "floatValue" parameter and set the "floatValue" parameter to default value (use "default" operator).
+        // TODO #18. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue"
+        // that gets "floatValue" parameter ("float" type) with ref keyword. The method should return value of
+        // "floatValue" parameter and set the "floatValue" parameter to default value (use "default" operator).
+        public static float ReturnParameterValueAndSetParameterToDefaultValue(ref float floatValue)
+        {
+            float defaultValue = default;
+            float returnValue = floatValue;
+            floatValue = defaultValue;
+            return returnValue;
+        }
 
-        // TODO #19. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue" that gets "intValue" parameter ("int" type) with ref keyword. The method should return value of "intValue" parameter and set the "intValue" parameter to default value (use "default" operator).
+        // TODO #19. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue" that gets
+        // "intValue" parameter ("int" type) with ref keyword. The method should return value of "intValue" parameter and
+        // set the "intValue" parameter to default value (use "default" operator).
+        public static int ReturnParameterValueAndSetParameterToDefaultValue(ref int intValue)
+        {
+            int defaultValue = default;
+            int returnValue = intValue;
+            intValue = defaultValue;
+            return returnValue;
+        }
 
-        // TODO #20. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue" that gets "longValue" parameter ("long" type) with ref keyword. The method should return value of "longValue" parameter and set the "longValue" parameter to default value (use "default" operator).
+        // TODO #20. Add a static method here with name "ReturnParameterValueAndSetParameterToDefaultValue"
+        // that gets "longValue" parameter ("long" type) with ref keyword. The method should return value of
+        // "longValue" parameter and set the "longValue" parameter to default value (use "default" operator).
+        public static long ReturnParameterValueAndSetParameterToDefaultValue(ref long longValue)
+        {
+            long defaultValue = default;
+            long returnValue = longValue;
+            longValue = defaultValue;
+            return returnValue;
+        }
     }
 }
